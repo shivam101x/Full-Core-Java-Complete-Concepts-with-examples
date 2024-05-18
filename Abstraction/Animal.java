581f011ca6b8a08fa75ae32691663da65aac6b84
Shivam <malvadeshivam@gmail.com>
2024-05-18 11:22:51 +0530

    Create Animal.java

---
+++ Abstraction/Animal.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+public abstract class Animal {
+	private String name;
+	
+	public Animal(String name) {
+		this.name=name;
+	}
+	 String getName() {
+		 return  name;
+	 }
+}
