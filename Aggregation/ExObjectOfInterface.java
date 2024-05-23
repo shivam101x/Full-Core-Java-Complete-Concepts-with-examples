65e59733fc968e689d4317af7baba8227a19542b
Shivam <malvadeshivam@gmail.com>
2024-05-23 11:55:19 +0530

    Create ExObjectOfInterface.java

---
+++ Aggregation/ExObjectOfInterface.java
@@ -0,0 +1,12 @@
+package Aggregation;
+
+import java.util.List;
+
+public class ExObjectOfInterface {
+	
+	public static void main(String[] args) {
+		
+//		List<Integer> li=new List<Integer>() {
+//		};
+	}
+}
