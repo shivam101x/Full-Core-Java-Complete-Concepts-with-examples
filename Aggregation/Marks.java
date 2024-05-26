de0f798211e5deac1692fb8cbe159de479c0e750
Shivam <malvadeshivam@gmail.com>
2024-05-26 11:42:55 +0530

    Create Marks.java

---
+++ Aggregation/Marks.java
@@ -0,0 +1,17 @@
+package Aggregation;
+
+public class Marks {
+	
+	int p,c,m,b;
+
+	public Marks(int p, int c, int m, int b) {
+		super();
+		this.p = p;
+		this.c = c;
+		this.m = m;
+		this.b = b;
+	}
+	
+	
+
+}
