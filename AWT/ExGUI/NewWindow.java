9040d396881039e8a3d3b62e8f7765ad75589621
Shivam <malvadeshivam@gmail.com>
2024-06-01 15:22:37 +0530

    Create NewWindow.java

---
+++ AWT/ExGUI/NewWindow.java
@@ -0,0 +1,14 @@
+package ExGUI;
+
+import javax.swing.JFrame;
+
+public class NewWindow extends JFrame{
+
+	public NewWindow() {
+		
+		setSize(600,600);
+		setLayout(null);
+		setVisible(true);
+	}
+
+}
