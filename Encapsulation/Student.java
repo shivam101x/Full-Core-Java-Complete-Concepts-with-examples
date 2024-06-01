20229c5eab3a8ff74b36072520cfe672bc3071c0
Shivam <malvadeshivam@gmail.com>
2024-06-01 15:25:36 +0530

    Create Student.java

---
+++ Encapsulation/Student.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+public class Student {
+	private int rollno,age;
+	private String name,address;
+	
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
