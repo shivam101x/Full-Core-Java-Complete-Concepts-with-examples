e7ba0203978ec36f0f15bc558752bd4391b94d72
Shivam <malvadeshivam@gmail.com>
2024-06-01 15:25:48 +0530

    Create ExWrapper.java

---
+++ Encapsulation/ExWrapper.java
@@ -0,0 +1,16 @@
+package Encapsulation;
+
+public class ExWrapper {
+
+	public static void main(String[] args) {
+		
+		Student ob=new Student();
+		ob.setRollno(101);
+		ob.setName("Abc");
+		ob.setAge(24);
+		ob.setAddress("Pune");
+//		System.out.println(ob.getRollno()+" "+ob.getName()+" "+ob.getAge()+" "+ob.getAddress());
+
+	}
+
+}
