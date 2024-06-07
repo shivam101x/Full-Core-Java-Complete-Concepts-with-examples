e74018fddef3c23facf782e36b305c0314a1bc89
Shivam <malvadeshivam@gmail.com>
2024-06-07 13:13:30 +0530

    Create ExString.java

---
+++ Ex_String/ExString.java
@@ -0,0 +1,30 @@
+package Ex_String;
+
+public class ExString{
+
+	public static void main(String[] args) {
+		String str="Welcome";
+		String str1=str.concat(" ABCD");
+//		System.out.println(str.indexOf("e"));
+		
+//		System.out.println(str);//immutable
+//		System.out.println(str1);
+//		for (int i = 0; i < str.length(); i++) {
+//			for (int j = 0; j <=i; j++) {
+//				System.out.print(str.charAt(j)+" ");
+//			}
+//			System.out.println();
+//		}
+//		String ab=str.trim();
+//		System.out.println(ab);
+//		System.out.println(str.startsWith(" "));
+		String st="Welcome";
+//		System.out.println(str.valueOf(ab+st));
+//		System.out.println(str.replace("come", "-done"));
+//		System.out.println(str.compareTo(st));
+//		System.out.println(str.codePointAt(0));
+		
+		System.out.println(st.toString());
+	}
+
+}
