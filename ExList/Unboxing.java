585d7203c1df3442c6bbc3a3b5961912b3f1b68b
Shivam <malvadeshivam@gmail.com>
2024-06-16 09:02:35 +0530

    Create Unboxing.java

---
+++ ExList/Unboxing.java
@@ -0,0 +1,24 @@
+package ExList;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		
+
+		Integer in2=50;
+		
+		int xy=in2;			//unboxing
+		
+		
+		Byte by=10;
+		Short sh=20;
+		Character ch=123;
+		Integer in=60;
+		Long lg=5647382476583746237l;
+		Float fl=90.90f;
+		Double dl=80.80;
+		Boolean bn = false;
+		
+	}
+
+}
