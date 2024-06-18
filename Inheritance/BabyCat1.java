42f1749fa802bd30cf88ea66876d653cdc4cb5a3
Shivam <malvadeshivam@gmail.com>
2024-06-18 11:22:38 +0530

    Create BabyCat1.java

---
+++ Inheritance/BabyCat1.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+public class BabyCat1 extends Cat {
+	
+	String color="White";
+	
+	void sound() {
+		System.out.println("chatters..");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
