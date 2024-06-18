d531c5af55e172fd52bb46ec4ea9843db2eb4bb5
Shivam <malvadeshivam@gmail.com>
2024-06-18 11:22:26 +0530

    Create Animal.java

---
+++ Inheritance/Animal.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+class Animal{
+	String color="brown";
+	String type="mammals";
+	
+	void eat() {
+		System.out.println("eating..");
+	}
+	void sleep() {
+		System.out.println("sleeping..");
+	}
+}
