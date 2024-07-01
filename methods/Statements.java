6cc17f3dc603e14e69f03e2542a2b46b6cdb8fd0
Shivam <malvadeshivam@gmail.com>
2024-07-01 12:03:44 +0530

    Create Statements.java

---
+++ methods/Statements.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class Statements {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			if (i==7) {
+				break;
+			}	
+			System.out.println(i);
+		}
+	}
+
+}
