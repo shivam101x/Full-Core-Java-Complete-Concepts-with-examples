88bda6654dc7db112124878bdf532c89312aa7ed
Shivam <malvadeshivam@gmail.com>
2024-07-03 21:46:55 +0530

    Create Demo_Object.java

---
+++ Object/Demo_Object.java
@@ -0,0 +1,23 @@
+package Object;
+
+
+class ABC{
+	
+}
+class Demo_Object extends ABC{
+
+	public static void main(String[] args) {
+		
+		Demo_Object ob1=new Demo_Object();
+		
+		Demo_Object ob2=new Demo_Object();
+		
+		Demo_Object ob3;
+		
+		Demo_Object ob4;
+		System.out.println("ABC");
+		
+		ABC ab=new Demo_Object();
+	}
+
+}
