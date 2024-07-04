91513304335faaea3224f141a70732fe42107d38
Shivam <malvadeshivam@gmail.com>
2024-07-04 12:53:14 +0530

    Create Marks.java

---
+++ Polymorphism/Marks.java
@@ -0,0 +1,17 @@
+package Polymorphism;
+
+public class Marks {
+	int p,c,m,b;
+	public Marks(int p, int c, int m, int b) {
+		super();
+		this.p = p;
+		this.c = c;
+		this.m = m;
+		this.b = b;
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
